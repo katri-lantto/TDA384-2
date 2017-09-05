63ace2a689518f691664842477c9322f607c3ca8
bob <robin.lilius@gmail.com>
2017-09-05 11:12:58 +0200

    Add: speed is saved in Train object

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -1,25 +1,32 @@
 import TSim.*;
+import java.util.concurrent.*;
 
 public class Lab1 {
   public TSimInterface tsi = TSimInterface.getInstance();
 
   public int[][] switchIDs = {
-    {17, 7},            // upper station
-    {15, 9},            // meeting section, east
-    {4, 9},             // meeting section, west
-    {3, 11}             // lower station
+    {17, 7},            // 0 upper station
+    {15, 9},            // 1 meeting section, east
+    {4, 9},             // 2 meeting section, west
+    {3, 11}             // 3 lower station
   };
 
   public int[][] sensorIDs = {
-    {13, 3}, {13, 5},   // upper station halt
-    {15, 7}, {15, 8},   // upper station switch
-    {18, 7}, {16, 9},   // east critical section
-    {14, 9}, {14, 10},  // meeting section, east switch
-    {5, 9}, {5, 10},    // meeting section, west switch
-    {3, 9}, {2, 11},    // west critical section
-    {5, 11}, {4, 13},   // lower station switch
-    {13, 11}, {13, 13}  // lower station halt
+    {13, 3}, {13, 5},   // 0,1 upper station halt
+    {15, 7}, {15, 8},   // 2,3 upper station switch
+    {18, 7}, {16, 9},   // 4,5 east critical section
+    {14, 9}, {14, 10},  // 6,7 meeting section, east switch
+    {5, 9}, {5, 10},    // 8,9 meeting section, west switch
+    {3, 9}, {2, 11},    // 10,11 west critical section
+    {5, 11}, {4, 13},   // 12,13 lower station switch
+    {13, 11}, {13, 13}  // 14,15 lower station halt
   };
+
+  public Semaphore semUpperStation = new Semaphore(1);
+  public Semaphore semLowerStation = new Semaphore(1);
+  public Semaphore semWestCritical = new Semaphore(1);
+  public Semaphore semEastCritical = new Semaphore(1);
+  public Semaphore semMiddle = new Semaphore(1);
 
   public Lab1(Integer speed1, Integer speed2) {
     try {
@@ -27,12 +34,19 @@ public class Lab1 {
       setSwitch(1, tsi.SWITCH_RIGHT);
 
       // tsi.setSpeed(1,speed1);
-      tsi.setSpeed(2,speed2);
+      // tsi.setSpeed(2,speed2);
 
-      Thread trainOne = new Thread(new Train(1));
-      Thread trainTwo = new Thread(new Train(2));
-      trainOne.start();
-      trainTwo.start();
+      Train train1 = new Train(1);
+      Train train2 = new Train(2);
+
+      train1.setSpeed(speed1);
+      train2.setSpeed(speed2);
+
+      Thread trainThread1 = new Thread(train1);
+      Thread trainThread2 = new Thread(train2);
+
+      trainThread1.start();
+      trainThread2.start();
     }
     catch (CommandException e) {
       e.printStackTrace();    // or only e.getMessage() for the error
@@ -54,18 +68,29 @@ public class Lab1 {
 
   class Train implements Runnable {
     private int id;
+    private int speed;
 
     public Train (int id) {
       this.id = id;
+    }
+
+    public void setSpeed(int speed) throws CommandException {
+      this.speed = speed;
+      tsi.setSpeed(id,speed);
     }
 
     public void run() {
       try {
 
         while (true) {
-          SensorEvent s1 = tsi.getSensor(id);
-          // System.out.println("is sensor 0: "+isSensor(s1, 0));
+          SensorEvent s = tsi.getSensor(id);
 
+          if ((isSensor(s, 0) || isSensor(s, 1)) && speed != 0) {
+            // int previousSpeed = speed;
+            // setSpeed(0);
+            // Thread.sleep(1000);
+            // setSpeed(-previousSpeed);
+          }
         }
       } catch (CommandException e) {
         e.printStackTrace();
